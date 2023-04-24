7fef5b519250e3cfc0322cff91d3394d69e9daa5
bebo13133 <borislaviliev47@gmail.com>
2023-04-24 12:46:20 +0300

    up

---
+++ about-borislav/app.js
@@ -0,0 +1,45 @@
+
+
+function openTab(tabname) {
+    var linnks = document.getElementsByClassName("active")
+    var contents = document.getElementsByClassName("education-content")
+    for (let link of linnks) {
+        link.classList.remove("active")
+    }
+    for (let content of contents) {
+        content.classList.remove("active-content")
+    };
+    event.currentTarget.classList.add("active");
+    document.getElementById(tabname).classList.add("active-content")
+};
+
+function openMenu() {
+    let menuSide = document.getElementById("burgerMenu")
+    menuSide.style.right = "0"
+
+}
+
+function closeMenu() {
+    let menuSide = document.getElementById("burgerMenu")
+    menuSide.style.right = "-220px"
+
+}
+
+
+let loader = document.getElementById('preloader')
+window.addEventListener("load", function () {
+    loader.style.display = "none"
+})
+var iconMode = document.getElementById("sunIcon");
+var logo = document.getElementById('logo-dark')
+iconMode.onclick = function(){
+    document.body.classList.toggle("light-mode");
+    if(document.body.classList.contains("light-mode")){
+        sunIcon.src = "/about-borislav/images/moon-6689.png";
+        logo.src="/about-borislav/images/logo-light.png"
+    }else{
+        sunIcon.src="/about-borislav/images/sun-8728.png";
+        logo.src="./images/logo-dark.png" 
+    }
+}
+
