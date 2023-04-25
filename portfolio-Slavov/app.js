468d708d9636e961e7131e8ea9c5d2f460f30ce3
bebo13133 <borislaviliev47@gmail.com>
2023-04-25 18:13:52 +0300

    created

---
+++ portfolio-Slavov/app.js
@@ -0,0 +1,38 @@
+
+
+function openTab(tabname) {
+    var linnks = document.getElementsByClassName("active")
+    var contents = document.getElementsByClassName("education-content")
+    for (let link of linnks) {
+        link.classList.remove("active")
+    }
+    for (let content of contents) {
+        content.classList.remove("active-content")
+    };
+    event.currentTarget.classList.add("active");
+    document.getElementById(tabname).classList.add("active-content")
+};
+
+function openMenu() {
+    let menuSide = document.getElementById("burgerMenu")
+    menuSide.style.right = "0"
+
+}
+
+function closeMenu() {
+    let menuSide = document.getElementById("burgerMenu")
+    menuSide.style.right = "-318px"
+
+}
+
+
+
+var iconMode = document.getElementById("sunIcon");
+var logo = document.getElementById('logo-dark')
+iconMode.onclick = function(){
+    document.body.classList.toggle("light-mode");
+    (document.body.classList.contains("light-mode")) ? (sunIcon.src = "/about-borislav/images/moon-6689.png", logo.src = "/about-borislav/images/logo-light.png")
+    : (sunIcon.src = "/about-borislav/images/sun-8728.png", logo.src = "./images/logo-dark.png")
+    }
+
+
