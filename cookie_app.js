29d4385e00cddbce99c85f2a972da6b71361db96
bebo13133 <borislaviliev47@gmail.com>
2023-04-30 21:49:30 +0300

    modify
    create cookie_app.js
    change cookie style

---
+++ cookie_app.js
@@ -0,0 +1,28 @@
+let wrapText = document.querySelector("#cookies")
+document.querySelector("#cookies_btn")
+ executedCode = () => {
+    if( document.cookie.includes("cookie")) return;
+    wrapText.classList.add("show");
+
+};     
+window.addEventListener("load", executedCode);
+
+setCookie = (userName, userValue, expDays) => {
+    let date = new Date();
+    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = userName + "=" + userValue + ";" + expires + "; path=/";
+}
+getCookie = (userName) => {
+    let nameInfo = userName + "=";
+    let usersInfo = decodeURIComponent(document.cookie);
+    let userArr = usersInfo.split("; ");
+    let value;
+    userArr.forEach(val => { if (val.indexOf(nameInfo) === 0) value = val.substring(name.length) })
+    return value;
+}
+document.querySelector("#cookies_btn").addEventListener('click', () => {
+        wrapText.classList.remove("show")
+       
+        setCookie("cookie", true, 15);
+    });
--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
 window.onload = function () {
-    var menuBox = document.getElementById('menuBox')
-    var icon = document.getElementById("menuIcon")
+    let menuBox = document.getElementById('menuBox')
+    let icon = document.getElementById("menuIcon")
 
     icon.onclick = () => {
         let x = document.querySelector('.drop-menu')
@@ -26,29 +26,10 @@ menuElements.forEach((elements) => {
 });
 // /============== COOKIESE==========================/
 
-setCookie = (userName, userValue, expDays) => {
-    let date = new Date();
-    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = userName + "=" + userValue + ";" + expires + "; path=/";
-}
 
-getCookie = (userName) => {
-    let name = userName + "=";
-    let usersInfo = decodeURIComponent(document.cookie);
-    let userArr = usersInfo.split("; ");
-    let value;
-    userArr.forEach(val => { if (val.indexOf(name) === 0) value = val.substring(name.length) })
-    return value;
-}
 
-document.querySelector("#cookies_btn").addEventListener('click', () => {
-    document.querySelector("#cookies").style.display = 'none';
-    setCookie("cookie", true, 15);
+// cMessage = () => {
+//     if (getCookie("cookie")) document.querySelector("#cookies").style.display = "block"
 
-})
-cMessage = () => {
-    if (!getCookie("cookie")) document.querySelector("#cookies").style.display = "block"
-
-};
-window.addEventListener("load", cMessage);
+// };
+// window.addEventListener("load", cMessage);
