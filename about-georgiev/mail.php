4a9615b128545d3d71868c5d47b5e6c7b1c11050
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-05-24 19:44:52 +0300

    update&addNikolayGeorgiev

---
+++ about-georgiev/mail.php
@@ -0,0 +1,16 @@
+<?php
+//get data from form  
+$name = $_POST['Name'];
+$email= $_POST['email'];
+$message= $_POST['Messages'];
+$to = "asya.georgieva@ruso.superhosting.bg";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@bebodesigns.eu" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
